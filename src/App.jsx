6877bd55486d29f878dadebb2cafa4057137a349
Thrisha Burra <thrishagoud023@gmail.com>
2025-12-19 13:23:54 +0530

    added some changes into git

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { Button } from '@/components/ui/button'
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router'
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
 // import AppwriteAccount from './appwrite/AccountServices'
 
-
-function App() {
-
-
+function App() { 
   return (
     <>
-   
-<h1>App .jsx basic home route</h1>
+      <h1>App .jsx basic home route</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
